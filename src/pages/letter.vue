fc4aea11e40583cdccba87b16da111f98d8715ea
Your Nameexit <you@example.com>
2023-05-30 14:34:08 +0700

    first commit

---
+++ src/pages/letter.vue
@@ -0,0 +1,54 @@
+<template>
+    <h3 class="text-2xl font-bold text-left py-2">Letter Type Information</h3>
+      <div class="section">
+        <div class="container">
+          <div class="columns">
+            <div class="column is-5 is-offset-3">
+              <form @submit.prevent="handleSubmit" class="py-3 px-10 text-left">
+                <tr>
+                  <label > Letter Name</label>
+                  <textarea class="bg-gray-200"
+                                        placeholder ="    Surat Keterangan"
+                                        rows="2"
+                                        cols="170"
+                                        v-model="form.name"
+                                    /><br>
+                 <label> Format</label>
+                  <textarea class="bg-gray-200 "
+                                        placeholder="    xxx/S.Pk/BKJ/mm/YYYY"
+                                        rows="2"
+                                        cols="170"
+                                        v-model="form.format"
+                                    /><br>
+                 <label> Restart Num Typle</label>
+                  <textarea class= 'bg-gray-200'
+                                      placeholder="    Monthly"
+                                        rows="2"
+                                        cols="170"
+                                        v-model="form.restart"
+                                    />
+                          <br>
+                <div class="field is-grouped">
+                </div>
+              </tr>
+              </form>
+            </div>
+            </div>
+          </div>
+        </div>
+    
+    </template>
+    
+    <script>
+     export default {
+      data() {
+            return {
+                text: "",
+                form: {
+                    lowongan: null,
+                    deskripsi: null,
+                },
+            };
+        },
+      }
+    </script>
---
+++ src/pages/profile.vue
@@ -0,0 +1,66 @@
+<template>
+<h3 class="text-2xl font-bold text-left py-2">Form lowongan</h3>
+  <div class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-5 is-offset-3">
+          <form @submit.prevent="handleSubmit" class="py-3 px-10 text-center">
+            <tr>
+              <textarea class="bg-gray-200"
+                                    placeholder="    Title"
+                                    rows="1"
+                                    cols="170"
+                                    v-model="form.title"
+                                /><br>
+              <textarea class="bg-gray-200"
+                                    placeholder="    Summary"
+                                    rows="1"
+                                    cols="170"
+                                    v-model="form.summary"
+                                /><br>
+              <textarea class= 'bg-gray-200'
+                                  placeholder="    Deskripsi"
+                                    rows="4"
+                                    cols="170"
+                                    v-model="form.deskripsi"
+                                />
+                      <br>
+              <textarea class="bg-gray-200"
+                                    placeholder="    Linkedin"
+                                    rows="1"
+                                    cols="170"
+                                    v-model="form.Linkedin"
+                                /><br>
+              <textarea class="bg-gray-200"
+                                    placeholder="    Glints link"
+                                    rows="1"
+                                    cols="170"
+                                    v-model="form.Glints"
+                                /><br><br>
+            <div class="field is-grouped">
+              <div class="control">
+                <button type="submit" class="button is-link bg-gray-400 px-4">Submit</button>
+              </div>
+            </div>
+          </tr>
+          </form>
+        </div>
+        </div>
+      </div>
+    </div>
+
+</template>
+
+<script>
+ export default {
+  data() {
+        return {
+            text: "",
+            form: {
+                lowongan: null,
+                deskripsi: null,
+            },
+        };
+    },
+  }
+</script>
